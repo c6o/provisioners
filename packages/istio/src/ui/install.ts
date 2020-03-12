5ac454c2be4c9d600ce295df8d5bfabeeef9b248
Adrian Jones <adrian@traxitt.com>
2020-03-12 09:51:06 -0700

    Implement a framework for global/local theming (#217)
    * upgrade to vaadin 15
    
    * required components layout
    
    * checkbox group
    
    * global and component styles
    
    * revert vaadin 15
    
    * Use the checkHandler method

--- packages/istio/src/ui/install.ts
+++ packages/istio/src/ui/install.ts
@@ -1,8 +1,17 @@
-import { LitElement, html, customElement } from 'lit-element'
+import { LitElement, html, customElement, css } from 'lit-element'
 import { StoreFlowStep, StoreFlowMediator } from '@provisioner/common'
 
 @customElement('istio-install-main')
 export class IstioSetup extends LitElement implements StoreFlowStep {
+
+    static get styles() {
+        return css`
+            traxitt-horizontal-layout > traxitt-form-layout {
+                flex-grow: 1;
+                flex-basis: 0;
+            }
+        `
+    }
 
     mediator: StoreFlowMediator
     get serviceSpec() {
@@ -11,93 +20,41 @@ export class IstioSetup extends LitElement implements StoreFlowStep {
 
     render() {
         return html`
-            <style>
-                .form-group {
-                    display: block;
-                    padding-bottom: var(--lumo-space-m);
-                }
-            </style>
-
-            <traxitt-form-layout>
-                <div class="container">
-                    <h4>Required Components:</h3>
-
-                    <div class="form-group">
-                        <traxitt-checkbox checked disabled>
-                            Istio Base (CRDs)
-                        </traxitt-checkbox>
-                    </div>
-                    <div class="form-group">
-                        <traxitt-checkbox checked disabled>
-                            Gateway (Ingress)
-                        </traxitt-checkbox>
-                    </div>
-                    <div class="form-group">
-                        <traxitt-checkbox checked disabled>
-                            Pilot (Traffic Management)
-                        </traxitt-checkbox>
-                    </div>
-                </div>
-
-                <div class="container">
-                    <h4>Optional Components:</h3>
-
-                    <div class="form-group">
-                        <traxitt-checkbox @checked-changed=${this.autoInjectCheckChanged} ?checked=${this.serviceSpec.autoInjectEnabled}>
-                            Auto Injection
-                        </traxitt-checkbox>
-                    </div>
-                    <div class="form-group">
-                        <traxitt-checkbox @checked-changed=${this.citadelCheckChanged} ?checked=${this.serviceSpec.citadelEnabled}>
-                            Citadel (Authentication and Identity)
-                        </traxitt-checkbox>
-                    </div>
-                    <div class="form-group">
-                        <traxitt-checkbox @checked-changed=${this.coreDnsCheckChanged} ?checked=${this.serviceSpec.coreDnsEnabled}>
-                            Core DNS
-                        </traxitt-checkbox>
-                    </div>
-                    <div class="form-group">
-                        <traxitt-checkbox @checked-changed=${this.galleyCheckChanged} ?checked=${this.serviceSpec.galleyEnabled}>
-                            Galley (Configuration)
-                        </traxitt-checkbox>
-                    </div>
-                    <div class="form-group">
-                        <traxitt-checkbox @checked-changed=${this.policyCheckChanged} ?checked=${this.serviceSpec.policyEnabled}>
-                            Policy
-                        </traxitt-checkbox>
-                    </div>
-                    <div class="form-group">
-                        <traxitt-checkbox @checked-changed=${this.telemetryCheckChanged} ?checked=${this.serviceSpec.telemetryEnabled}>
-                            Telemetry (Analytics)
-                        </traxitt-checkbox>
-                    </div>
-                </div>
-            </traxitt-form-layout>
+            <traxitt-horizontal-layout>
+                <traxitt-form-layout>
+                    <h4>Optional Components:</h4>
+                    <traxitt-checkbox @change=${this.checkHandler('autoInjectEnabled')} ?checked=${this.serviceSpec.autoInjectEnabled}>
+                        Auto Injection
+                    </traxitt-checkbox>
+                    <traxitt-checkbox @change=${this.checkHandler('citadelEnabled')} ?checked=${this.serviceSpec.citadelEnabled}>
+                        Citadel (Authentication and Identity)
+                    </traxitt-checkbox>
+                    <traxitt-checkbox @change=${this.checkHandler('coreDnsEnabled')} ?checked=${this.serviceSpec.coreDnsEnabled}>
+                        Core DNS
+                    </traxitt-checkbox>
+                    <traxitt-checkbox @change=${this.checkHandler('galleyEnabled')} ?checked=${this.serviceSpec.galleyEnabled}>
+                        Galley (Configuration)
+                    </traxitt-checkbox>
+                    <traxitt-checkbox @change=${this.checkHandler('policyEnabled')} ?checked=${this.serviceSpec.policyEnabled}>
+                        Policy
+                    </traxitt-checkbox>
+                    <traxitt-checkbox @change=${this.checkHandler('telemetryEnabled')} ?checked=${this.serviceSpec.telemetryEnabled}>
+                        Telemetry (Analytics)
+                    </traxitt-checkbox>
+                </traxitt-form-layout>
+                <traxitt-form-layout>
+                    <h4>Required Components:</h4>
+                    <ul>
+                        <li>Istio Base (CRDs)</li>
+                        <li>Gateway (Ingress)</li>
+                        <li>Pilot (Traffic Management)</li>
+                    </ul>
+                </traxitt-form-layout>
+            </traxitt-horizontal-layout>
         `
     }
 
-    autoInjectCheckChanged = (e) => {
-        this.serviceSpec.autoInjectEnabled = e.detail.value
-    }
-
-    citadelCheckChanged = (e) => {
-        this.serviceSpec.citadelEnabled = e.detail.value
-    }
-
-    coreDnsCheckChanged = (e) => {
-        this.serviceSpec.coreDnsEnabled = e.detail.value
-    }
-
-    galleyCheckChanged = (e) => {
-        this.serviceSpec.galleyEnabled = e.detail.value
-    }
-
-    policyCheckChanged = (e) => {
-        this.serviceSpec.policyEnabled = e.detail.value
-    }
-
-    telemetryCheckChanged = (e) => {
-        this.serviceSpec.telemetryEnabled = e.detail.value
+    checkHandler = (field) => (e) => {
+        this.serviceSpec[field] = e.detail.value
     }
 }
