99a456312132b0c9e59e12d29a8c250e69f43c54
Mike Blackstock <mike@traxitt.com>
2020-06-03 10:10:26 -0700

    editions tests (#520)
    * simplify createInquire
    
    * unit tests for retrieving editions and manifests
    
    * add to admin permissions list

--- packages/vscode/src/createInquire.ts
+++ packages/vscode/src/createInquire.ts
@@ -16,59 +16,38 @@ function resolvePath(filePath: string) {
 
 export const createInquireMixin = (base: baseProvisionerType) => class extends base {
 
-    providedStorageSetting(answers) {
-        return this.spec.storage || answers['storage']
+    pubKeyPath(args) {
+        return this.spec.keyFile || args['pubKeyPath'] || '~/.ssh/id_rsa.pub'
     }
 
-    providedDevEnvSetting(answers) {
-        return this.spec.env || answers['env']
-    }
+    async createInquire(args) {
 
-    pubKeyPath(answers) {
-        return this.spec.keyFile || answers['pubKeyPath'] || '~/.ssh/id_rsa.pub'
-    }
-
-    async createInquire(answers) {
-
-        if (!this.providedStorageSetting(answers)) {
-            const response = await this.manager.inquirer.prompt({
-                type: 'input',
-                name: 'storage',
-                default: '4Gi',
-                message: 'What size /data volume would you like?'
-            })
-
-            if (response)
-                this.spec.storage = response.storage
-            else
-                this.spec.storage = '4Gi'
-        } else this.spec.storage =this.providedStorageSetting(answers)
-    
-        let devEnv:string
-        if (!this.providedDevEnvSetting(answers)) {
-            const response = await this.manager.inquirer.prompt({
-                type: 'list',
-                name: 'env',
-                message: 'What environment do you want?',
-                choices: ['node','go'],
-                default: 0
-            })
-
-            if (response)
-                devEnv = response.env
-            else
-                devEnv = 'node'
-
+        const answers = {
+            storage: args.storage || this.spec.storage || '4Gi',
         }
-        this.spec.img =`traxitt/${devEnv}-dev`
+
+        const response = await this.manager.inquirer.prompt([{
+            type: 'input',
+            name: 'storage',
+            default: '4Gi',
+            message: 'What size data volume would you like for VSCode?',
+            askAnswered: true
+        }, {
+            type: 'list',
+            name: 'env',
+            message: 'What environment do you want?',
+            choices: ['node', 'go'],
+            default: 0
+        }], answers)
+
+        this.spec.img = `traxitt/${response.env}-dev`
 
         if (!this.spec.publicKey) {
-            this.spec.publicKey = await fs.readFile(resolvePath(this.pubKeyPath(answers)), 'utf8')
+            this.spec.publicKey = await fs.readFile(resolvePath(this.pubKeyPath(args)), 'utf8')
             if (!this.spec.publicKey)
                 throw new Error('publicKey is required')
         }
 
         this.spec.launch = !!this.spec.launch
-
     }
 }
