1679881a616a7cd3f9bab1d37ca4aab339e4abae
Narayan Sainaney <narayan@codezero.io>
2020-08-12 16:16:36 -0700

    chore: Working with 2 namespaces

--- packages/mattermost/src/mixins/createApply.ts
+++ packages/mattermost/src/mixins/createApply.ts
@@ -62,77 +62,31 @@ export const createApplyMixin = (base: baseProvisionerType) => class extends bas
                 .end()
         } else {
 
-            await this.manager.cluster
-                .begin(`Installing mattermost services ('${edition}' edition.)`)
-                //.list(this.mattermostClusterPods)
-                .do((result, processor) => {
-                    //no-op
-                })
-                .end()
+            mySqlconfig.metadata.namespace = 'mysql-operator'//namespace
 
-            // await this.manager.cluster
-            //     .begin('Installing mysql operator')
-            //     .list(this.mattermostClusterPods)
-            //     .do((result, processor) => {
-            //         if (!result?.object?.items?.length) {
-            //             processor
-            //                 .upsertFile('../../k8s/full/1-mysql-operator.yaml')
-            //         }
-            //     })
-            //     .end()
             await this.manager.cluster
                 .begin('Install mysql operator')
-                .upsertFile('../../k8s/full/1-mysql-operator.yaml')
+                    .upsert(mySqlconfig)
+                    .upsertFile('../../k8s/full/1-mysql-operator.yaml', { namespace: 'mysql-operator' })
                 .end()
-
-            // await this.manager.cluster
-            //     .begin('Installing minio operator')
-            //     .list(this.mattermostClusterPods)
-            //     .do((result, processor) => {
-            //         if (!result?.object?.items?.length) {
-            //             processor
-            //                 .upsertFile('../../k8s/full/2-minio-operator.yaml')
-            //         }
-            //     })
-            //     .end()
 
 
             await this.manager.cluster
                 .begin('Install minio operator')
-                .upsertFile('../../k8s/full/2-minio-operator.yaml')
+                    .upsertFile('../../k8s/full/2-minio-operator.yaml', { namespace })
                 .end()
 
-            // await this.manager.cluster
-            //     .begin('Installing Mattermost Operator')
-            //     .list(this.mattermostClusterPods)
-            //     .do((result, processor) => {
-            //         if (!result?.object?.items?.length) {
-            //             processor
-            //                 .upsertFile('../../k8s/full/3-mattermost-operator.yaml')
-            //         }
-            //     })
-            //     .end()
-
+            debugger
             await this.manager.cluster
                 .begin('Install mattermost operator')
-                .upsertFile('../../k8s/full/3-mattermost-operator.yaml')
+                    .addOwner(this.manager.document)
+                    .upsertFile('../../k8s/full/3-mattermost-operator.yaml', { namespace })
                 .end()
-
-            // await this.manager.cluster
-            //     .begin('Installing Mattermost Cluster')
-            //     .list(this.mattermostClusterPods)
-            //     .do((result, processor) => {
-            //         if (!result?.object?.items?.length) {
-            //             processor
-            //                 .addOwner(this.manager.document)
-            //                 .upsertFile('../../k8s/full/4-mattermost-cluster.yaml', { namespace, name, users, mattermostLicenseSecret, databaseStorageSize, minioStorageSize })
-            //         }
-            //     })
-            //     .end()
 
             await this.manager.cluster
                 .begin('Install mattermost cluster')
-                .upsertFile('../../k8s/full/4-mattermost-cluster.yaml', { namespace, name, users, mattermostLicenseSecret, databaseStorageSize, minioStorageSize })
+                    .addOwner(this.manager.document)
+                    .upsertFile('../../k8s/full/4-mattermost-cluster.yaml', { namespace, name, users, mattermostLicenseSecret, databaseStorageSize, minioStorageSize })
                 .end()
         }
     }
@@ -149,4 +103,20 @@ export const createApplyMixin = (base: baseProvisionerType) => class extends bas
             })
             .end()
     }
+}
+
+const mySqlconfig: any = {
+	"apiVersion": "v1",
+	"kind": "ConfigMap",
+	"metadata": {
+		"name": "mysql-operator-orc",
+		"labels": {
+			"app": "mysql-operator",
+			"release": "mysql-operator"
+		}
+	},
+	"data": {
+		"orchestrator.conf.json": "{\n  \"ApplyMySQLPromotionAfterMasterFailover\": false,\n  \"BackendDB\": \"sqlite\",\n  \"Debug\": false,\n  \"DetachLostReplicasAfterMasterFailover\": true,\n  \"DetectClusterAliasQuery\": \"SELECT CONCAT(SUBSTRING(@@hostname, 1, LENGTH(@@hostname) - 1 - LENGTH(SUBSTRING_INDEX(@@hostname,'-',-2))),'.',SUBSTRING_INDEX(@@report_host,'.',-1))\",\n  \"DetectInstanceAliasQuery\": \"SELECT @@hostname\",\n  \"DiscoverByShowSlaveHosts\": false,\n  \"FailMasterPromotionIfSQLThreadNotUpToDate\": true,\n  \"HTTPAdvertise\": \"http://{{ .Env.HOSTNAME }}-svc:80\",\n  \"HostnameResolveMethod\": \"none\",\n  \"InstancePollSeconds\": 5,\n  \"ListenAddress\": \":3000\",\n  \"MasterFailoverDetachReplicaMasterHost\": true,\n  \"MySQLHostnameResolveMethod\": \"@@report_host\",\n  \"MySQLTopologyCredentialsConfigFile\": \"/etc/orchestrator/orc-topology.cnf\",\n  \"OnFailureDetectionProcesses\": [\n    \"/usr/local/bin/orc-helper event -w '{failureClusterAlias}' 'OrcFailureDetection' 'Failure: {failureType}, failed host: {failedHost}, lost replcas: {lostReplicas}' || true\",\n    \"/usr/local/bin/orc-helper failover-in-progress '{failureClusterAlias}' '{failureDescription}' || true\"\n  ],\n  \"PostIntermediateMasterFailoverProcesses\": [\n    \"/usr/local/bin/orc-helper event '{failureClusterAlias}' 'OrcPostIntermediateMasterFailover' 'Failure type: {failureType}, failed hosts: {failedHost}, slaves: {countSlaves}' || true\"\n  ],\n  \"PostMasterFailoverProcesses\": [\n    \"/usr/local/bin/orc-helper event '{failureClusterAlias}' 'OrcPostMasterFailover' 'Failure type: {failureType}, new master: {successorHost}, slaves: {slaveHosts}' || true\"\n  ],\n  \"PostUnsuccessfulFailoverProcesses\": [\n    \"/usr/local/bin/orc-helper event -w '{failureClusterAlias}' 'OrcPostUnsuccessfulFailover' 'Failure: {failureType}, failed host: {failedHost} with {countSlaves} slaves' || true\"\n  ],\n  \"PreFailoverProcesses\": [\n    \"/usr/local/bin/orc-helper failover-in-progress '{failureClusterAlias}' '{failureDescription}' || true\"\n  ],\n  \"ProcessesShellCommand\": \"sh\",\n  \"RaftAdvertise\": \"{{ .Env.HOSTNAME }}-svc\",\n  \"RaftBind\": \"{{ .Env.HOSTNAME }}\",\n  \"RaftDataDir\": \"/var/lib/orchestrator\",\n  \"RaftEnabled\": true,\n  \"RaftNodes\": [],\n  \"RecoverIntermediateMasterClusterFilters\": [\n    \".*\"\n  ],\n  \"RecoverMasterClusterFilters\": [\n    \".*\"\n  ],\n  \"RecoveryIgnoreHostnameFilters\": [],\n  \"RecoveryPeriodBlockSeconds\": 300,\n  \"RemoveTextFromHostnameDisplay\": \":3306\",\n  \"SQLite3DataFile\": \"/var/lib/orchestrator/orc.db\",\n  \"SlaveLagQuery\": \"SELECT TIMESTAMPDIFF(SECOND,ts,NOW()) as drift FROM sys_operator.heartbeat ORDER BY drift ASC LIMIT 1\",\n  \"UnseenInstanceForgetHours\": 1\n}",
+		"orc-topology.cnf": "[client]\nuser = {{ .Env.ORC_TOPOLOGY_USER }}\npassword = {{ .Env.ORC_TOPOLOGY_PASSWORD }}\n"
+	}
 }
--- packages/mattermost/src/mixins/removeApply.ts
+++ packages/mattermost/src/mixins/removeApply.ts
@@ -8,8 +8,8 @@ export const removeApplyMixin = (base: baseProvisionerType) => class extends bas
         // TODO: this needs more work since the preview may have been installed
         await this.manager.cluster
             .begin('Deprovisioning the app')
-                .deleteFile('../../k8s/full/1-mysql-operator.yaml')
-                .deleteFile('../../k8s/full/2-minio-operator.yaml')
+                .deleteFile('../../k8s/full/1-mysql-operator.yaml', { namespace: 'mysql-operator' })
+                .deleteFile('../../k8s/full/2-minio-operator.yaml', { namespace: 'minio-operator' })
                 .deleteFile('../../k8s/full/3-mattermost-operator.yaml')
                 .deleteFile('../../k8s/full/4-mattermost-cluster.yaml', { namespace })
             .end()
