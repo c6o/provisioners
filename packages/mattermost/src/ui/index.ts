f22c85c2f31966a478e495f98033d8130f1012e2
Narayan Sainaney <narayan@codezero.io>
2020-08-12 18:52:40 -0700

    chore: Added validation to UI

--- packages/mattermost/src/ui/index.ts
+++ packages/mattermost/src/ui/index.ts
@@ -1,4 +1,5 @@
 import { LitElement, html, customElement } from 'lit-element'
+import { ComboBoxElement } from '@vaadin/vaadin-combo-box/src/vaadin-combo-box'
 import { StoreFlowStep, StoreFlowMediator } from '@provisioner/common'
 
 @customElement('mattermost-install-main')
@@ -6,17 +7,22 @@ export class MattermostSettings extends LitElement implements StoreFlowStep {
     mediator: StoreFlowMediator
 
     //these choices are based on the mattermost enterprise operator/deployment
-    storageSizeChoices = ['5Gi', '10Gi', '50Gi', '100Gi', '200Gi', '400Gi', '1000Gi']
+    storageSizeChoices = ['2Gi', '5Gi', '10Gi', '50Gi', '100Gi', '200Gi', '400Gi', '1000Gi']
     userCountChoices = [100, 1000, 5000, 10000, 25000]
 
     get serviceSpec() {
         return this.mediator.getServiceSpec('mattermost')
     }
 
+    get userCountList() { return this.shadowRoot.getElementById('userCountList') as ComboBoxElement }
+    get databaseSizeList() { return this.shadowRoot.getElementById('databaseSizeList') as ComboBoxElement }
+    get minioSizeList() { return this.shadowRoot.getElementById('minioSizeList') as ComboBoxElement }
+
     render() {
         return html`
             <c6o-form-layout>
                 <c6o-combo-box
+                    id="userCountList"
                     @selected-item-changed=${this.usersSelected}
                     error-message="Please select an expected number of users"
                     value="100"
@@ -32,26 +38,29 @@ export class MattermostSettings extends LitElement implements StoreFlowStep {
                 ></c6o-text-field>
 
                 <c6o-combo-box
+                    id="databaseSizeList"
                     @selected-item-changed=${this.dbSizeSelected}
-                    error-message="Please select an expected size."
+                    error-message="Please select a database size."
                     value="2Gi"
                     required
-                    label="Amount of storage to provision for the database"
+                    label="Amount of storage for the database"
                     .items=${this.storageSizeChoices}
                 ></c6o-combo-box>
 
                 <c6o-combo-box
+                    id="minioSizeList"
                     @selected-item-changed=${this.minioSizeSelected}
                     error-message="Please select an expected size."
                     value="2Gi"
                     required
-                    label="Amount of storage to provision for minio"
+                    label="Amount of storage for media files"
                     .items=${this.storageSizeChoices}
                 ></c6o-combo-box>
 
         </c6o-form-layout>
         `
     }
+
     minioSizeSelected = (e) => {
         this.serviceSpec.minioStorageSize = e.detail.value
     }
@@ -71,5 +80,28 @@ export class MattermostSettings extends LitElement implements StoreFlowStep {
         if (edition !== 'latest')
             throw new Error('Only the latest edition of Mattermost Provisioner allowed to pass')
         this.serviceSpec.edition = edition
+    }
+
+    async end() {
+        let valid = true
+        await window.customElements.whenDefined('c6o-combo-box')
+
+        if (!this.serviceSpec.users) {
+            this.userCountList.invalid = true
+            valid = false
+        }
+
+        if (!this.serviceSpec.databaseStorageSize) {
+            this.databaseSizeList.invalid = true
+            valid = false
+        }
+
+        if (!this.serviceSpec.minioStorageSize) {
+            this.minioSizeList.invalid = true
+            valid = false
+        }
+
+        this.requestUpdate()
+        return valid
     }
 }
