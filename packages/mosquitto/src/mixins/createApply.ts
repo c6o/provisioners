0dd1d3e6a42977541843c1671d5972b15338408a
Rob Chartier <rob@codezero.io>
2020-08-13 16:43:34 -0700

    initial mosquitto/mqtt provisioner layout

---
+++ packages/mosquitto/src/mixins/createApply.ts
@@ -0,0 +1,57 @@
+import { baseProvisionerType } from '../index'
+import { Buffer } from 'buffer'
+export const createApplyMixin = (base: baseProvisionerType) => class extends base {
+
+    get mosquittoPods() {
+        return {
+            kind: 'Pod',
+            metadata: {
+                namespace: this.serviceNamespace,
+                labels: {
+                    app: 'mosquitto'
+                }
+            }
+        }
+    }
+
+    async createApply() {
+        await this.ensureServiceNamespacesExist()
+        await this.installMosquitto()
+        await this.ensureMosquittoIsRunning()
+    }
+
+    async installMosquitto() {
+        const namespace = this.serviceNamespace
+
+        await this.manager.cluster
+            .begin('Install mosquitto deployment')
+                .addOwner(this.manager.document)
+                .upsert(this.mySqlConfig)
+                .upsertFile('../../k8s/latest/1-deployment', { namespace })
+            .end()
+
+
+        await this.manager.cluster
+            .begin('Install NodePort')
+                .addOwner(this.manager.document)
+                .upsertFile('../../k8s/latest/2-nodeport.yaml', { namespace })
+            .end()
+
+        await this.manager.cluster
+            .begin('Install Virtual Service')
+                .addOwner(this.manager.document)
+                .upsertFile('../../k8s/latest/3-virtualservice.yaml', { namespace })
+            .end()
+
+    }
+
+    async ensureMosquittoIsRunning() {
+        await this.manager.cluster.
+            begin('Ensure mosquitto services are running')
+                .beginWatch(this.MattermostmosquittoPods)
+                .whenWatch(({ condition }) => condition.Ready === 'True', (processor) => {
+                    processor.endWatch()
+                })
+            .end()
+    }
+}
---
+++ packages/mosquitto/src/mixins/createValidate.ts
@@ -0,0 +1,8 @@
+import { baseProvisionerType } from '../index'
+
+export const createValidateMixin = (base: baseProvisionerType) => class extends base {
+
+    async createValidate() {
+
+    }
+}
