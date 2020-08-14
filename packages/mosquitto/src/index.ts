50e84c7195b335bd691ebc6ece33695640c2241d
Narayan Sainaney <narayan@codezero.io>
2020-08-14 11:27:42 -0700

    Skleton of add and remove user updateApply

--- packages/mosquitto/src/index.ts
+++ packages/mosquitto/src/index.ts
@@ -2,11 +2,13 @@ import { mix } from "mixwith"
 import { ProvisionerBase } from "@provisioner/common"
 
 import {
+    userMgmtMixin,
     createApplyMixin,
     createInquireMixin,
     createValidateMixin,
     removeApplyMixin,
-    removeInquireMixin
+    removeInquireMixin,
+    updateApplyMixin
 } from './mixins'
 
 export type baseProvisionerType = new (...a) => Provisioner & ProvisionerBase
@@ -14,6 +16,6 @@ export type baseProvisionerType = new (...a) => Provisioner & ProvisionerBase
 export interface Provisioner extends ProvisionerBase {
 }
 
-export class Provisioner extends mix(ProvisionerBase).with(createApplyMixin, createInquireMixin, createValidateMixin, removeApplyMixin, removeInquireMixin) {
+export class Provisioner extends mix(ProvisionerBase).with(createApplyMixin, createInquireMixin, createValidateMixin, removeApplyMixin, removeInquireMixin, userMgmtMixin, updateApplyMixin) {
     get isLatest() { return this.edition === 'latest' }
 }
