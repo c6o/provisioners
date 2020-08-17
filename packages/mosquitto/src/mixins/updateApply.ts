0e6405f325675acb92f755507c0cb33812558ea9
Rob Chartier <rob@codezero.io>
2020-08-17 15:07:32 -0700

    Add, remove, update users all supported

--- packages/mosquitto/src/mixins/updateApply.ts
+++ packages/mosquitto/src/mixins/updateApply.ts
@@ -6,7 +6,7 @@ export const updateApplyMixin = (base: baseProvisionerType) => class extends bas
         const newUser = this.spec['add-user']
         if (newUser) {
             this.manager.status?.push('Adding user to Mosquitto')
-            await this.addUser(newUser.user, newUser.password, appNamespace)
+            await this.addUser(newUser.user, newUser.password, appNamespace, true)
             this.manager.document.spec.provisioner['add-user'] = '$unset'
             this.manager.status?.pop()
         }
@@ -17,8 +17,19 @@ export const updateApplyMixin = (base: baseProvisionerType) => class extends bas
 
         if (removeUser) {
             this.manager.status?.push('Removing user from Mosquitto')
-            await this.removeUser(removeUser.user, appNamespace)
+            await this.removeUser(removeUser.user, appNamespace, true)
             this.manager.document.spec.provisioner['remove-user'] = '$unset'
+            this.manager.status?.pop()
+        }
+    }
+
+    async updateUpdateUser(appNamespace) {
+        const updateUser = this.spec['update-user']
+
+        if (updateUser) {
+            this.manager.status?.push('Updating user in Mosquitto')
+            await this.updateUser(updateUser.originalUsername, updateUser.newUsername, updateUser.password, appNamespace)
+            this.manager.document.spec.provisioner['update-user'] = '$unset'
             this.manager.status?.pop()
         }
     }
@@ -29,5 +40,6 @@ export const updateApplyMixin = (base: baseProvisionerType) => class extends bas
 
         await this.updateAddUser(appNamespace)
         await this.updateRemoveUser(appNamespace)
+        await this.updateUpdateUser(appNamespace)
     }
 }
