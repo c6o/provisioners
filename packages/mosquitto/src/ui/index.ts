acdcf7b5e0415a4ef6bb419e8eb0d9957410da45
Rob Chartier <rob@codezero.io>
2020-08-20 14:01:36 -0700

    missed a debugger statement

--- packages/mosquitto/src/ui/index.ts
+++ packages/mosquitto/src/ui/index.ts
@@ -28,7 +28,6 @@ export class MosquittoSettings extends LitElement implements StoreFlowStep {
 
     async begin() {
         // set defaults
-        debugger
         const edition = this.mediator.applicationSpec.metadata.labels['system.codezero.io/edition']
         this.serviceSpec.edition = edition
     }
