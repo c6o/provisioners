91d8a1fa33fb8a8ec55368148b674c397bc131f6
Rob Chartier <rob@codezero.io>
2020-08-20 11:56:12 -0700

    mosquitto working end to end via CLI and UI provisioners. (other than TCP services)

--- packages/mosquitto/src/index.ts
+++ packages/mosquitto/src/index.ts
@@ -17,51 +17,7 @@ export interface Provisioner extends ProvisionerBase {
 }
 
 export class Provisioner extends mix(ProvisionerBase).with(createApplyMixin, createInquireMixin, createValidateMixin, removeApplyMixin, removeInquireMixin, userMgmtMixin, updateApplyMixin) {
+
     get isLatest() { return this.edition === 'latest' }
 
-    async getConfigMap(namespace: string, name: string) {
-
-        const manifest = {
-            apiVersion: 'v1',
-            kind: 'ConfigMap',
-            metadata: {
-                name,
-                namespace
-            }
-        }
-
-        const result = await this.manager.cluster.read(manifest)
-
-        if (result.error) {
-            throw new Error(`Failed to load the ConfigMap '${name}' from '${namespace}'`)
-        }
-
-        return { configmap: result.object, manifest }
-    }
-
-    async restartDeployment(namespace: string, name: string) {
-
-        const manifest = {
-            apiVersion: 'apps/v1',
-            kind: 'Deployment',
-            metadata: {
-                name,
-                namespace
-            }
-        }
-        const deploymentResult = await this.manager.cluster.read(manifest)
-
-        if (deploymentResult.error) {
-            throw new Error(`Failed to retreive deployment '${name}' from '${namespace}'`)
-        }
-
-        const deployment = deploymentResult.object
-
-        //grab the current replica count
-        const previousCount = deployment.spec?.replicas || 0
-        //set replicas to 0, that will trigger a shutdown
-        await this.manager.cluster.patch(deployment, { spec: { replicas: 0 } })
-        //set replicas back to what it was, that will trigger a startup
-        await this.manager.cluster.patch(deployment, { spec: { replicas: previousCount } })
-    }
 }
