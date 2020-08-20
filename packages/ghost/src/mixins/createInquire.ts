cb26cfaf7d09543512353b1ce7c1ac1e2a3a4bba
Rob Chartier <rob@codezero.io>
2020-08-20 13:59:45 -0700

    initial ghost provisioner

---
+++ packages/ghost/src/mixins/createInquire.ts
@@ -0,0 +1,9 @@
+import { baseProvisionerType } from '../index'
+
+export const createInquireMixin = (base: baseProvisionerType) => class extends base {
+
+    async createInquire(args) {
+
+
+    }
+}
