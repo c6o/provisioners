2c6b491909c5da025b587ce0941f41420d4756c1
Rob Chartier <rob@codezero.io>
2020-08-24 15:13:35 -0700

    mosquitto provisioner

---
+++ packages/mosquitto/src/index.ts
@@ -0,0 +1,45 @@
+import { mix } from "mixwith"
+import { ProvisionerBase } from "@provisioner/common"
+
+import {
+    userMgmtMixin,
+    createApplyMixin,
+    createInquireMixin,
+    createValidateMixin,
+    removeApplyMixin,
+    updateApplyMixin
+} from './mixins'
+
+export type baseProvisionerType = new (...a) => Provisioner & ProvisionerBase
+
+export interface Provisioner extends ProvisionerBase {
+}
+
+export class Provisioner extends mix(ProvisionerBase).with(createApplyMixin, createInquireMixin, createValidateMixin, removeApplyMixin, userMgmtMixin, updateApplyMixin) {
+
+    get isLatest() { return this.edition === 'latest' }
+
+
+    async getSettingsConfigMap(namespace: string, name: string) {
+
+        const manifest = {
+            apiVersion: 'v1',
+            kind: 'ConfigMap',
+            metadata: {
+                name,
+                namespace
+            }
+        }
+
+        const result = await this.manager.cluster.read(manifest)
+
+        if (result.error) {
+            throw new Error(`Failed to load the ConfigMap '${name}' from '${namespace}'`)
+        }
+
+        return { configmap: result.object, manifest }
+    }
+
+
+
+}
