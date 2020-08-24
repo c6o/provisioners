9190467af6aff7f493c53bfde0d20ca98791e259
Rob Chartier <rob@codezero.io>
2020-08-24 14:10:38 -0700

    PR review changes

--- packages/ghost/src/mixins/createValidate.ts
+++
@@ -1,9 +0,0 @@
-import { baseProvisionerType } from '../index'
-
-export const createValidateMixin = (base: baseProvisionerType) => class extends base {
-
-    async createValidate() {
-
-
-    }
-}
