0a011d46992a13016d02433691f0d44099d0c052
Rob Chartier <rob@codezero.io>
2020-08-25 15:54:19 -0700

    feat(mosquitto): Mosquitto provisioner (#40)
    * mosquitto provisioner
    
    * updated parent package.json
    
    * PR review changes

---
+++ packages/mosquitto/src/index.ts
@@ -0,0 +1,44 @@
+import { mix } from "mixwith"
+import { ProvisionerBase } from "@provisioner/common"
+
+import {
+    userMgmtMixin,
+    createApplyMixin,
+    createInquireMixin,
+    createValidateMixin,
+    updateApplyMixin
+} from './mixins'
+
+export type baseProvisionerType = new (...a) => Provisioner & ProvisionerBase
+
+export interface Provisioner extends ProvisionerBase {
+}
+
+export class Provisioner extends mix(ProvisionerBase).with(createApplyMixin, createInquireMixin, createValidateMixin, userMgmtMixin, updateApplyMixin) {
+
+    get isLatest() { return this.edition === 'latest' }
+
+
+    async getSettingsConfigMap(namespace: string, name: string) {
+
+        const manifest = {
+            apiVersion: 'v1',
+            kind: 'ConfigMap',
+            metadata: {
+                name,
+                namespace
+            }
+        }
+
+        const result = await this.manager.cluster.read(manifest)
+
+        if (result.error) {
+            throw new Error(`Failed to load the ConfigMap '${name}' from '${namespace}'`)
+        }
+
+        return { configmap: result.object, manifest }
+    }
+
+
+
+}
---
+++ packages/mosquitto/src/mixins/api/users.ts
@@ -0,0 +1,109 @@
+import { baseProvisionerType } from '../../'
+import mosquittoPasswd from 'mosquitto-passwd'
+import parse from 'parse-passwd'
+
+declare module '../../' {
+    export interface Provisioner {
+        addUser(username: string, password: string, namespace: string, restart: boolean)
+        removeUser(username: string, namespace: string, restart: boolean)
+        updateUser(originalUsername: string, newUsername: string, password: string, namespace: string)
+        listUsers(namespace: string)
+    }
+}
+
+export const userMgmtMixin = (base: baseProvisionerType) => class extends base {
+
+    //taken from the yaml/deployment/metadata/name
+    deploymentName = 'mosquitto'
+
+    //taken from the yaml/configmap/metadata/name
+    configMapName = 'mosquitto-config'
+
+    async listUsers(namespace: string) {
+        const settings = await this.readSettings(namespace)
+        return settings.users
+    }
+
+    async parse(passwdPayload: string) {
+        //returns {Array}: Array of user objects parsed from the content.
+        return parse(passwdPayload)
+    }
+    async generateMosquittoUserPayload(username: string, password: string) {
+        // Username, password, salt (optional, by default will generate 12 bytes salt)
+        return await mosquittoPasswd(username, password)
+    }
+
+    async readSettings(namespace: string) {
+
+        const settings: any = await this.getSettingsConfigMap(namespace, this.configMapName)
+
+        settings.mosquittoConf = settings.configmap.data['mosquitto.conf']
+        settings.userConf = settings.configmap.data['users.conf']
+        settings.users = await this.parse(settings.userConf)
+
+        return settings
+    }
+
+    async updateUser(originalUsername: string, newUsername: string, password: string, namespace: string) {
+        await this.removeUser(originalUsername, namespace, false)
+        await this.addUser(newUsername, password, namespace, true)
+    }
+    async addUser(username: string, password: string, namespace: string, restart: boolean) {
+
+        const settings = await this.readSettings(namespace)
+
+        const found = settings.users.find(user => user.username === username) !== undefined
+
+        if (!found) {
+
+            //use 3rd party modules to generate our username:password bits
+            const userPayload = await this.generateMosquittoUserPayload(username, password)
+
+            //add the new credential to the existing value
+            settings.userConf += ('\n' + userPayload).trim()
+
+            //update our config file/map to the full value
+            //probably could condese all this down to a single, ugly line; but readability is important!
+            settings.configmap.data['users.conf'] = settings.userConf
+
+            //update our cluster/config map to reflect the change
+            const updatedResult = await this.manager.cluster.put(settings.manifest, settings.configmap)
+
+            if (updatedResult.error) {
+                throw new Error('Failed to save Mosquitto password configMap')
+            }
+
+            if (restart) {
+                //kick the deployment for the new settings to take effect
+                await super.restartDeployment(namespace, this.deploymentName)
+            }
+        }
+
+    }
+
+    async removeUser(username: string, namespace: string, restart: boolean) {
+        const settings = await this.readSettings(namespace)
+        const found = settings.users.find(user => user.username === username) !== undefined
+        if (found) {
+
+            //filter the item from our array
+            const users = settings.users.filter(item => item.username !== username)
+
+            //join the reamining items and stuff them back into our config map
+            settings.configmap.data['users.conf'] = users.map(item => `${item.username}:${item.password}`).join('\n').trim()
+
+            //update our cluster/config map to reflect the change
+            const updatedResult = await this.manager.cluster.put(settings.manifest, settings.configmap)
+
+            if (updatedResult.error) {
+                throw new Error('Failed to save Mosquitto password configMap')
+            }
+
+            if (restart) {
+                //kick the deployment for the new settings to take effect
+                await super.restartDeployment(namespace, this.deploymentName)
+            }
+
+        }
+    }
+}
