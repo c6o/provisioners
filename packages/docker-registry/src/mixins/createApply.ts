446aacbe3a1dfc929bc6c4c678e3b1a14c153a22
Rob Chartier <rob@codezero.io>
2020-09-01 14:28:53 -0700

    initial layout for docker-registry provisioner

---
+++ packages/docker-registry/src/mixins/createApply.ts
@@ -0,0 +1,68 @@
+import { baseProvisionerType } from '../index'
+export const createApplyMixin = (base: baseProvisionerType) => class extends base {
+
+    get dockerRegistryPods() {
+        return {
+            kind: 'Pod',
+            metadata: {
+                namespace: this.serviceNamespace,
+                labels: {
+                    app: 'docker-registry'
+                }
+            }
+        }
+    }
+
+    async createApply() {
+        await this.ensureServiceNamespacesExist()
+        await this.installMosquitto()
+        await this.ensureMosquittoIsRunning()
+    }
+
+    async installMosquitto() {
+        const namespace = this.serviceNamespace
+
+        const {
+            username,
+            password,
+        } = this.spec
+
+        //we will always need to send in "users" to the configMap
+        //so we set it to empty string to start with
+        let users = ''
+        //if we have a username && password
+        if (username?.length > 0 && password?.length > 0) {
+            users = await this.generateMosquittoUserPayload(username, password)
+        }
+
+        await this.manager.cluster
+            .begin('Install mosquitto deployment')
+            .addOwner(this.manager.document)
+            .upsertFile('../../k8s/latest/1-deployment.yaml', { namespace, users })
+            .end()
+
+
+        await this.manager.cluster
+            .begin('Install NodePort')
+            .addOwner(this.manager.document)
+            .upsertFile('../../k8s/latest/2-nodeport.yaml', { namespace })
+            .end()
+
+        await this.manager.cluster
+            .begin('Install Virtual Service')
+            .addOwner(this.manager.document)
+            .upsertFile('../../k8s/latest/3-virtualservice.yaml', { namespace })
+            .end()
+
+    }
+
+    async ensureMosquittoIsRunning() {
+        await this.manager.cluster.
+            begin('Ensure mosquitto services are running')
+            .beginWatch(this.dockerRegistryPods)
+            .whenWatch(({ condition }) => condition.Ready === 'True', (processor) => {
+                processor.endWatch()
+            })
+            .end()
+    }
+}
