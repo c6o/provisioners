e04735b8dd318883f1f6fcd888f976b255f113f5
Rob Chartier <rob@codezero.io>
2020-09-02 09:16:31 -0700

    feat(suitecrm): SuiteCRM provisioner (#47)
    * Initial bitnami / suitecrm provisioner
    
    * final changes for the UI provisioner
    
    * Update packages/suitecrm/src/ui/index.ts
    
    Co-authored-by: Grant Burnard <grant@traxitt.com>
    
    * Update packages/suitecrm/src/mixins/createInquire.ts
    
    Co-authored-by: Grant Burnard <grant@traxitt.com>
    
    * Update packages/suitecrm/src/mixins/createInquire.ts
    
    Co-authored-by: Grant Burnard <grant@traxitt.com>
    
    * Update packages/suitecrm/src/ui/index.ts
    
    Co-authored-by: Grant Burnard <grant@traxitt.com>
    
    * removed uncessary config value
    
    Co-authored-by: Grant Burnard <grant@traxitt.com>

---
+++ packages/suitecrm/src/ui/index.ts
@@ -0,0 +1,45 @@
+import { LitElement, html, customElement } from 'lit-element'
+import { ComboBoxElement } from '@vaadin/vaadin-combo-box/src/vaadin-combo-box'
+import { StoreFlowStep, StoreFlowMediator } from '@provisioner/common'
+
+@customElement('suitecrm-install-main')
+export class SuiteCRMSettings extends LitElement implements StoreFlowStep {
+
+    mediator: StoreFlowMediator
+    storageSizeChoices = ['1Gi','2Gi', '5Gi', '10Gi', '50Gi', '100Gi', '200Gi', '400Gi', '1000Gi']
+
+    get serviceSpec() {
+        return this.mediator.getServiceSpec('suitecrm')
+    }
+    render() {
+        return html`
+            <c6o-form-layout>
+                <c6o-text-field @input=${this.usernameChanged} label="Administrator username" value=${this.serviceSpec.suitecrmusername} autoselect required></c6o-text-field>
+                <br />
+                <c6o-password-field @input=${this.passwordChanged} label="Administrator password" value=${this.serviceSpec.suitecrmpassword} autoselect required></c6o-password-field>
+            </c6o-form-layout>
+            <c6o-form-layout>
+                <c6o-combo-box @selected-item-changed=${this.storageSelected} label="Database Storage" value=${this.serviceSpec.storage} required allow-custom-value .items=${this.storageSizeChoices}></c6o-combo-box>
+            </c6o-form-layout>
+        `
+    }
+
+    async begin() {
+        this.serviceSpec.storage = this.serviceSpec.storage || '1Gi'
+        this.serviceSpec.suitecrmusername = this.serviceSpec.suitecrmusername || 'admin'
+        this.serviceSpec.suitecrmpassword = this.serviceSpec.suitecrmpassword || 'admin'
+    }
+
+    storageSelected = (e) => {
+        this.serviceSpec.storage = e.detail.value
+    }
+
+    usernameChanged = (e) => {
+        this.serviceSpec.suitecrmusername = e.target.value
+    }
+
+    passwordChanged = (e) => {
+        this.serviceSpec.suitecrmpassword = e.target.value
+    }
+
+}
