c76a49fc5c0da0458a7f8a3215dc65a1cab61d53
Rob Chartier <rob@codezero.io>
2021-02-24 16:25:20 -0800

    rolled back changes

--- packages/appengine/src/ui/section.ts
+++ packages/appengine/src/ui/section.ts
@@ -1,4 +1,4 @@
-import { LitElement, customElement, html, property, eventOptions } from 'lit-element'
+import { LitElement, customElement, html, property } from 'lit-element'
 import { AppEngineAppObject, PromptType, Section } from '@provisioner/appengine-contracts'
 
 @customElement('appengine-section')
@@ -13,10 +13,6 @@ export class AppEngineSection extends LitElement {
     @property({ type: Object })
     manifestHelper: AppEngineAppObject
 
-    updateRequested = () => {
-        this.requestUpdate()
-    }
-
     get renderPrompts() {
         return this.section?.prompts || this.prompts
     }
@@ -27,24 +23,31 @@ export class AppEngineSection extends LitElement {
                 ${this.renderTitle()}
                 <c6o-form-layout>
                     ${Array.isArray(this.renderPrompts) ?
-                    this.renderPrompts.map(prompt => {
-                        return html`<appengine-prompt
-                            @update-requested=${this.updateRequested}
+                        this.renderPrompts.map(prompt => {
+                            return html`
+                                <appengine-prompt
+                                    .answers=${this.manifestHelper.answers}
+                                    .document=${this.manifestHelper.document}
+                                    .prompt=${prompt}
+                                    @update-requested=${this.updateRequested}
+                                ></appengine-prompt>
+                            `
+                        })
+                    : html`
+                        <appengine-prompt
                             .answers=${this.manifestHelper.answers}
                             .document=${this.manifestHelper.document}
-                            .prompt=${prompt}>
-                        </appengine-prompt>`
-                    }) :
-                    html`<appengine-prompt
-                        @update-requested=${this.updateRequested}
-                        .answers=${this.manifestHelper.answers}
-                        .document=${this.manifestHelper.document}
-                        .prompt=${this.renderPrompts}>
-                    </appengine-prompt>`
-                }
+                            .prompt=${this.renderPrompts}
+                            @update-requested=${this.updateRequested}
+                        ></appengine-prompt>
+                    `}
                 </c6o-form-layout>
             `
     }
 
     renderTitle = () => this.section?.title ? html`<h2>${this.section.title}</h2>` : ''
+
+    updateRequested = () => {
+        this.requestUpdate()
+    }
 }
