f382c2e0cb492e6e22baec4b951657e70ccd78a2
Narayan Sainaney <narayan@codezero.io>
2021-06-07 14:58:12 -0700

    Added static toKeyValues

--- packages/common/src/helpers/configMap.ts
+++ packages/common/src/helpers/configMap.ts
@@ -1,15 +1,25 @@
 import { Cluster, keyValue } from '@c6o/kubeclient-contracts'
-import { ConfigMap } from '@c6o/kubeclient-resources/core/v1'
+import { ConfigMap, ConfigMapList } from '@c6o/kubeclient-resources/core/v1'
 import { ConfigMapHelper as ConfigMapHelperContract } from '@provisioner/contracts'
 
 export class ConfigMapHelper<T extends ConfigMap = ConfigMap> extends ConfigMapHelperContract<T> {
 
+    resourceList: ConfigMapList
+
     static from = (namespace?: string, name?: string) =>
         new ConfigMapHelper(ConfigMapHelper.template(namespace, name))
+
+    static toKeyValues(configs: ConfigMap[], merge: keyValue = {}): keyValue {
+        return configs.reduce((acc, config:ConfigMap) => {
+            if (!config.data) return acc
+            return { ...acc, ...config.data }
+        }, merge)
+    }
 
     async toKeyValues(cluster: Cluster, merge: keyValue | Promise<keyValue> = {}) {
         const result = await cluster.read(this.resource)
         result.throwIfError()
+        this.resourceList = result.as<ConfigMapList>()
         return result.object.items.reduce((acc, config:ConfigMap) => {
             if (!config.data) return acc
             return { ...acc, ...config.data }
