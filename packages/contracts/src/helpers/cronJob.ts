f6741b53737821d31349afa0fb1e66788cb25b49
Grant Burnard <grant@traxitt.com>
2021-07-09 09:50:43 -0700

    feature(contracts): Added helpers for other workloads (#259)

---
+++ packages/contracts/src/helpers/cronJob.ts
@@ -0,0 +1,18 @@
+import { CodeZeroHelper } from "../codezero"
+import { CronJob } from '@c6o/kubeclient-resources/batch/v1beta1'
+
+export class CronJobHelper<T extends CronJob = CronJob>
+    extends CodeZeroHelper<T> {
+
+    static template = (namespace?: string, name?: string): CronJob => ({
+        apiVersion: 'batch/v1beta1',
+        kind: 'CronJob',
+        metadata: {
+            ...(name ? { name } : undefined),
+            ...(namespace ? { namespace } : undefined)
+        }
+    })
+
+    static from = (namespace?: string, name?: string) =>
+        new CronJobHelper(CronJobHelper.template(namespace, name))
+}
---
+++ packages/contracts/src/helpers/pod.ts
@@ -0,0 +1,18 @@
+import { CodeZeroHelper } from "../codezero"
+import { Pod } from '@c6o/kubeclient-resources/core/v1'
+
+export class PodHelper<T extends Pod = Pod>
+    extends CodeZeroHelper<T> {
+
+    static template = (namespace?: string, name?: string): Pod => ({
+        apiVersion: 'v1',
+        kind: 'Pod',
+        metadata: {
+            ...(name ? { name } : undefined),
+            ...(namespace ? { namespace } : undefined)
+        }
+    })
+
+    static from = (namespace?: string, name?: string) =>
+        new PodHelper(PodHelper.template(namespace, name))
+}
